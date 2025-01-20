56688d81e8d514c13fd071734e347ad9eef43bff
Alexandre Dos Reis <ajm.dosreis.daponte@gmail.com>
2025-01-20 15:39:16 +0100

    exit

--- src/main.zig
+++ src/main.zig
@@ -9,8 +9,14 @@ pub fn main() !void {
         var buffer: [1024]u8 = undefined;
         const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
 
-        var spansIterator = std.mem.splitSequence(u8, user_input, " ");
-        const command = spansIterator.next().?;
+        var iter = std.mem.splitSequence(u8, user_input, " ");
+        const command = iter.next().?;
+
+        if (std.mem.eql(u8, command, "exit")) {
+            const exitCode = std.fmt.parseInt(u8, iter.next().?, 10) catch 0;
+            std.process.exit(exitCode);
+        }
+
         stdout.print("{s}: command not found\n", .{command}) catch {};
     }
 }
