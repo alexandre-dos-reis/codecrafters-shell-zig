15f49056ad5d87a2e2cbebc7f24628b45494a6a7
Alexandre Dos Reis <ajm.dosreis.daponte@gmail.com>
2025-01-20 16:50:20 +0100

    echo

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,17 @@
 const std = @import("std");
 
+const Command = enum { exit, echo, notFound };
+
+fn parseCommand(input: []const u8) Command {
+    if (std.mem.eql(u8, input, "exit")) {
+        return Command.exit;
+    } else if (std.mem.eql(u8, input, "echo")) {
+        return Command.echo;
+    } else {
+        return Command.notFound;
+    }
+}
+
 pub fn main() !void {
     while (true) {
         const stdout = std.io.getStdOut().writer();
@@ -12,11 +24,17 @@ pub fn main() !void {
         var iter = std.mem.splitSequence(u8, user_input, " ");
         const command = iter.next().?;
 
-        if (std.mem.eql(u8, command, "exit")) {
-            const exitCode = std.fmt.parseInt(u8, iter.next().?, 10) catch 0;
-            std.process.exit(exitCode);
+        switch (parseCommand(command)) {
+            .exit => {
+                const exitCode = std.fmt.parseInt(u8, iter.next().?, 10) catch 0;
+                std.process.exit(exitCode);
+            },
+            .echo => {
+                try stdout.print("{s}\n", .{iter.rest()});
+            },
+            .notFound => {
+                stdout.print("{s}: command not found\n", .{command}) catch {};
+            },
         }
-
-        stdout.print("{s}: command not found\n", .{command}) catch {};
     }
 }
