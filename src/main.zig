197712ad2a5b55221a09b57f3cb830f9230858bf
Alexandre Dos Reis <ajm.dosreis.daponte@gmail.com>
2025-01-21 14:28:55 +0100

    trying to pass exec

--- src/main.zig
+++ src/main.zig
@@ -2,11 +2,24 @@ const std = @import("std");
 
 const Command = enum { exit, echo, type };
 
-fn parseCommand(input: []const u8) ?Command {
-    inline for (@typeInfo(Command).Enum.fields) |field| {
-        if (std.mem.eql(u8, input, field.name)) {
-            return @enumFromInt(field.value);
-        }
+fn findExecutablePath(externalCommand: []const u8) ?[]const u8 {
+    const envPaths = std.posix.getenv("PATH") orelse "/foo:/bar";
+
+    var pathIter = std.mem.splitSequence(u8, envPaths, ":");
+
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const allocator = arena.allocator();
+    arena.deinit();
+
+    while (pathIter.next()) |path| {
+        const binaryPath = std.fs.path.join(allocator, &[_][]const u8{ path, externalCommand }) catch continue;
+
+        std.fs.cwd().access(binaryPath, .{ .mode = .read_only }) catch |err| switch (err) {
+            error.FileNotFound => continue,
+            else => return null,
+        };
+
+        return binaryPath;
     }
     return null;
 }
@@ -22,12 +35,10 @@ pub fn main() !void {
 
         var iter = std.mem.splitSequence(u8, user_input, " ");
         const rawCommand = iter.next().?;
-        const nullishCommand = parseCommand(rawCommand);
 
-        if (nullishCommand == null) {
-            try stdout.print("{s}: not found\n", .{rawCommand});
-        } else {
-            switch (nullishCommand.?) {
+        // Builtins commands
+        if (std.meta.stringToEnum(Command, rawCommand)) |command| {
+            switch (command) {
                 .exit => {
                     const exitCode = std.fmt.parseInt(u8, iter.next().?, 10) catch 0;
                     std.process.exit(exitCode);
@@ -37,39 +48,34 @@ pub fn main() !void {
                 },
                 .type => {
                     const typeArg = iter.next().?;
-                    const optionalCommandFound = parseCommand(typeArg);
+                    const maybeCommand = std.meta.stringToEnum(Command, typeArg);
 
-                    if (optionalCommandFound != null) {
+                    if (maybeCommand) |_| {
                         try stdout.print("{s} is a shell builtin\n", .{typeArg});
                     } else {
-                        const envPaths = std.posix.getenv("PATH") orelse "/foo:/bar";
-
-                        var pathIter = std.mem.splitSequence(u8, envPaths, ":");
-
-                        var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-                        const allocator = arena.allocator();
-                        arena.deinit();
-
-                        var isFound = false;
-
-                        path: while (pathIter.next()) |path| {
-                            const binaryPath = try std.mem.concat(allocator, u8, &[_][]const u8{ path, "/", typeArg });
-
-                            std.fs.cwd().access(binaryPath, .{ .mode = .read_only }) catch |err| {
-                                if (err == error.FileNotFound) {
-                                    continue :path;
-                                }
-                            };
-
-                            try stdout.print("{s} is {s}\n", .{ typeArg, binaryPath });
-                            isFound = true;
-                            break :path;
-                        }
-                        if (!isFound) {
+                        if (findExecutablePath(typeArg)) |path| {
+                            try stdout.print("{s} is {s}\n", .{ typeArg, path });
+                        } else {
                             try stdout.print("{s}: not found\n", .{typeArg});
                         }
                     }
                 },
+            }
+            // External commands
+        } else {
+            if (findExecutablePath(rawCommand)) |path| {
+                var args = std.ArrayList([]const u8).init(std.heap.page_allocator);
+                defer args.deinit();
+
+                try args.append(path);
+
+                while (iter.next()) |arg| {
+                    try args.append(arg);
+                }
+                var childProcess = std.process.Child.init(args.items, std.heap.page_allocator);
+                _ = try childProcess.spawnAndWait();
+            } else {
+                try stdout.print("{s}: command not found\n", .{rawCommand});
             }
         }
     }
